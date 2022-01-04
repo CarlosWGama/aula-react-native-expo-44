de1fcb6065db2a4ef750bc11ce483d1d9485aa09
Carlos W. Gama <carloswgama@gmail.com>
2022-01-04 18:49:26 -0300

    Interface

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { Button, Input } from 'react-native-elements';
 import bg from './../../assets/imgs/background.png';
 
 export interface LoginscreenProps {
@@ -10,6 +11,23 @@ export function Loginscreen (props: LoginscreenProps) {
         <ImageBackground source={bg} style={styles.background}>
             <View style={styles.container}>
                 <Text style={styles.logo}>APP</Text>
+
+                {/* EMAIL  */}
+                <Input placeholder="Digite seu email" 
+                    inputContainerStyle={styles.containerInput}
+                    inputStyle={{color:'white'}}
+                    leftIcon={{name:'person', color: 'white'}} />
+
+                {/* SENHA */}
+                <Input placeholder="Digite sua senha" 
+                    inputContainerStyle={styles.containerInput}
+                    inputStyle={{color:'white'}}
+                    leftIcon={{name:'lock', color: 'white'}}
+                    secureTextEntry={true} />
+
+                <Button title="Logar" containerStyle={{borderRadius:30}} raised={true} />
+
+                <Text style={styles.cadastrar}>Não possui conta? Clique aqui para se cadastrar</Text>
             </View>
         </ImageBackground>
     );
@@ -23,5 +41,18 @@ const styles = StyleSheet.create({
         padding: 10,
         alignItems: 'stretch'
     },
-    logo: { color: 'white', fontSize: 50, textAlign: 'center'}
+    containerInput: {
+        backgroundColor: 'rgba(255, 255, 255, 0.3)',
+        borderRadius: 30,
+        padding: 5,
+        marginBottom: -10
+    },
+    logo: { color: 'white', fontSize: 50, textAlign: 'center'},
+    cadastrar: {
+        color: 'white',
+        fontSize: 20,
+        textDecorationLine: 'underline',
+        margin: 30,
+        textAlign: 'center'
+    }   
 });
